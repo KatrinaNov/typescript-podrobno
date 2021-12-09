5675d12d4a99b6bd886542431ec8f59b1f5f6690
katrina <katrina.nov13@gmail.com>
2021-12-09 15:32:45 +0300

    13. controlled input, checkbox, select

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -2,6 +2,7 @@ import React, {ChangeEvent, useRef, useState} from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { Button } from './Button';
+import {action} from "@storybook/addon-actions";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
@@ -22,4 +23,35 @@ export const GetValueOfUncontrolledInputbyButtonPress = () => {
     setValue(el.value)
   }
   return <><input ref={inputRef} /> <button onClick={save}>save</button> - actual value: {value} </>
+}
+
+export const ControlledInput = () => {
+  const [parentValue, setParentValue] = useState("")
+  const onChange = (e:ChangeEvent<HTMLInputElement>) => {
+    setParentValue(e.currentTarget.value)
+  }
+  return <><input value={parentValue} onChange={onChange} /></>
+}
+export const ControlledCheckbox = () => {
+  const [parentValue, setParentValue] = useState(true)
+  const onChange = (e:ChangeEvent<HTMLInputElement>) => {
+    setParentValue(e.currentTarget.checked)
+  }
+  return <><input type="checkbox" checked={parentValue} onChange={onChange} /></>
+
+}
+export const ControlledSelect = () => {
+  const [parentValue, setParentValue] = useState<string | undefined>("1")
+  const onChange = (e:ChangeEvent<HTMLSelectElement>) => {
+    setParentValue(e.currentTarget.value)
+  }
+  return <select value={parentValue} onChange={onChange}>
+    <option>Choose</option>
+    <option value="1">Minsk</option>
+    <option value="2">Kiev</option>
+    <option value="3">Moscow</option>
+
+  </select>
+
+
 }
