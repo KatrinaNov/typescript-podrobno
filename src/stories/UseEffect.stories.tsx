d0e356dacd76bfddb26171d824b59392ce78ed21
katrina <katrina.nov13@gmail.com>
2022-01-17 17:47:01 +0300

    21. useeffect/setTimeout, setInterval

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -31,3 +31,40 @@ export const Example1 = () => {
   </>
 }
 
+export const SetTimeOutExample = () => {
+  console.log('Example1')
+  const [fake, setFake] = useState(1);
+  const [counter, setCounter] = useState(1);
+
+  useEffect(() => {
+    console.log('useEffect every render')
+    setInterval(() => {
+      setCounter((state) => state + 1);
+    }, 1000)
+  }, [])
+
+
+  return <>
+    Hello, {counter} {fake}
+    {/*<button onClick={() => setCounter(counter  + 1)}>counter +</button>*/}
+    {/*<button onClick={() => setFake(fake  + 1)}>fake +</button>*/}
+  </>
+}
+export const Clock = () => {
+  const [date, setDate] = useState(new Date().toLocaleTimeString());
+
+  useEffect(() => {
+    console.log('useEffect every render')
+    setInterval(() => {
+      setDate(new Date().toLocaleTimeString());
+    }, 1000)
+  }, [])
+
+
+  return <>
+    Hello, {date}
+    {/*<button onClick={() => setCounter(counter  + 1)}>counter +</button>*/}
+    {/*<button onClick={() => setFake(fake  + 1)}>fake +</button>*/}
+  </>
+}
+
